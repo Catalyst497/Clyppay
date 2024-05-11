ad47584bbfc0e9f9b0cc24ec98dd1275e1e5ce14
Ebun <oyebolaebun07@gmail.com>
2024-05-11 11:01:20 +0100

    tailwind setup

--- src/components/ui/Card.jsx
+++ src/components/ui/Card.jsx
@@ -1,13 +1,17 @@
-export default function Card({ img, title, body }) {
+import image from "../../assets/image.png";
+
+export default function Card({ title, body }) {
   return (
-    <div id="card-container"
-     className = "bg-red-200  rounded-lg border bg-card text-card-foreground shadow-sm">
-      <div id="image-container">
-        <img src="'" alt="" />
+    <div
+      id="card-container"
+      className="bg-white w-[580px] rounded-lg border bg-card text-card-foreground shadow-sm"
+    >
+      <div id="image-container" className="bg-gradient">
+        <img src={image} alt="ya" />
       </div>
 
       <div>
-        <h2>{title}</h2>
+        <h2 className="text-7xl">{title}</h2>
         <p>{body}</p>
       </div>
       <div>{/* pagination here*/}</div>

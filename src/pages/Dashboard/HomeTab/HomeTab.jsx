2883fa8dbd39234065a4d32f54cb3726bc5ab9fe
Ebun <oyebolaebun07@gmail.com>
2024-05-31 05:14:12 +0100

    STARTED DASHBOARD

--- src/pages/Dashboard/HomeTab/HomeTab.jsx
+++ src/pages/Dashboard/HomeTab/HomeTab.jsx
@@ -1,45 +1,53 @@
 import React from "react"
-// import { QuickAction } from "@/components/dashboard/QuickAction"
+import { QuickAction } from "@/components/dashboard/QuickAction"
 
-// import SwipeCard from "@/components/dashboard/SwipeCard"
-// import AccountTabs from "@/components/dashboard/AccountTabs"
+import SwipeCard from "@/components/dashboard/SwipeCard"
+import AccountTabs from "@/components/dashboard/AccountTabs"
 // import TransactionHistory from "@/components/dashboard/TransactionHistory"
 
-// import Portfolio from "@/components/dashboard/Portfolio"
+import Portfolio from "@/components/dashboard/Portfolio"
 import { useAuth } from "@/context/AuthContext"
 import { ButtonPill } from "@/components/shared/shadcn/button"
+import {
+    Tabs,
+    TabsContent,
+    TabsList,
+    TabsTrigger,
+} from "@/components/shared/shadcn/tabs"
+import SwipeCard from "@/components/dashboard/SwipeCard"
 
 function HomeTab() {
     const { user } = useAuth()
 
     return (
-        <div>
-            <h2 className="text-lg pb-2">Hello {user?.name ?? "User"}</h2>
-            <div className="flex space-x-5">
-                <ButtonPill>Accounts</ButtonPill>
-                <ButtonPill>Cards</ButtonPill>
-                <ButtonPill>Savings</ButtonPill>
-            </div>
-        </div>
-        //     <div className="grid grid-cols-2 pb-10 space-x-10">
+        <Tabs defaultValue="account" className="w-full">
+            <h2 className="pb-2 text-lg">Hello {user?.name ?? "User"}</h2>
+            <TabsList className="flex space-x-5">
+                <TabsTrigger value="account">Account</TabsTrigger>
+                <TabsTrigger value="cards">Cards</TabsTrigger>
+                <TabsTrigger value="savings">Savings</TabsTrigger>
+            </TabsList>
 
-        //       <div className="col-span-1">
-
-        //         {" "}
-        //       <div>
-        //        <AccountTabs />
-        //         <SwipeCard />
-        //       </div>
-
-        //       <div className="">
-        //         <QuickAction />
-        //       </div>
-        //     </div>
-
-        //  <div className="col-span-1 max-w-md">
-        //       <Portfolio  />
-        //  </div>
-        //     </div>
+            <TabsContent value="account">
+                //{" "}
+                <div className="grid grid-cols-2 space-x-10 pb-10">
+                    <div className="col-span-1">
+                        {" "}
+                        <div>
+                  
+                            <SwipeCard />{" "}
+                        </div>
+                        <div className="">
+                            <QuickAction />
+                        </div>{" "}
+                    </div>
+                    <div className="col-span-1 max-w-md">
+                        {" "}
+                        <Portfolio />{" "}
+                    </div>{" "}
+                </div>
+            </TabsContent>
+        </Tabs>
     )
 }
 
